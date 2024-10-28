f3c4bb6685093bc397ce1ab6022ef322ef7c118a
yeonah_park <yeonah.park97@gmail.com>
2024-10-28 17:08:15 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 68.70 ms, Memory: 17.8 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
@@ -2,14 +2,11 @@ def solution(gems):
     total_gems = len(set(gems))  # 보석의 종류 수
     gem_count = {}  # 현재 구간의 보석 개수를 저장할 딕셔너리
     start, end = 0, 0
-    result = [0, len(gems) - 1]  # 최소 구간 초기값 (전체 구간으로 설정)
+    result = [0, len(gems)]  # 최소 구간 초기값 (전체 구간으로 설정)
     
     while end < len(gems):
         # end 포인터를 이동시키며 보석 추가
-        if gems[end] in gem_count:
-            gem_count[gems[end]] += 1
-        else:
-            gem_count[gems[end]] = 1
+        gem_count[gems[end]] = gem_count.get(gems[end], 0) + 1
         end += 1
         
         # 모든 종류의 보석을 포함하는 구간이 될 때까지 start 포인터 이동
@@ -24,5 +21,5 @@ def solution(gems):
                 del gem_count[gems[start]]  # 개수가 0이 되면 해당 보석 제거
             start += 1
     
-    # 진열대 번호는 1번부터 시작하므로 결과에 +1
+    # 진열대 번호는 1번부터 시작하므로 결과에 +2
     return [result[0] + 1, result[1] + 1]
