2761841332d8109bab7d5f51a39098729ac25bc0
yeonah_park <yeonah.park97@gmail.com>
2024-11-04 19:53:55 +0900

    [level 2] Title: 롤케이크 자르기, Time: 769.64 ms, Memory: 56.3 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -1,7 +1,6 @@
 from collections import deque
 
 def solution(topping):
-    # 철수가 처음에 모든 토핑을 가지고 시작합니다.
     cheolsu = {}
     for t in topping:
         if t in cheolsu:
@@ -16,17 +15,17 @@ def solution(topping):
 
     # 토핑을 하나씩 옮겨가면서 확인
     while topping:
-        # 철수의 토핑에서 하나를 뺍니다.
-        t = topping.popleft()
-        cheolsu[t] -= 1
+        # 철수의 토핑에서 하나빼서
+        t = topping.popleft()  # t에 저장
+        cheolsu[t] -= 1 #해당 토핑 갯수 줄이기
         if cheolsu[t] == 0:
-            del cheolsu[t]
+            del cheolsu[t] #개수가 0이 되면 딕셔너리에서 해당 토핑 제거
 
-        # 동생의 토핑에 추가합니다.
+        # 동생의 토핑에 추가
         if t in bro:
-            bro[t] += 1
+            bro[t] += 1 #이미 해당 토핑이 있다면 개수 증가
         else:
-            bro[t] = 1
+            bro[t] = 1 #없었다면 1 새로 추가
 
         # 공평하게 나눠진 경우를 체크
         if len(cheolsu) == len(bro):
@@ -37,4 +36,3 @@ def solution(topping):
             break
 
     return answer
-
