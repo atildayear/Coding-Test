ebf23977d76d2dfa439d580faae2b4101c6a5522
yeonah_park <yeonah.park97@gmail.com>
2024-11-04 20:06:21 +0900

    [level 2] Title: 롤케이크 자르기, Time: 626.08 ms, Memory: 50.3 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -1,38 +1,19 @@
-from collections import deque
+from collections import Counter
 
 def solution(topping):
-    cheolsu = {}
-    for t in topping:
-        if t in cheolsu:
-            cheolsu[t] += 1
-        else:
-            cheolsu[t] = 1
-    
-    # 동생의 토핑 딕셔너리 초기화
-    bro = {}
-    topping = deque(topping)
     answer = 0
+    bro = set()             # 동생이 가진 토핑 종류
+    cheolsu = Counter(topping)  # 전체 토핑의 종류와 개수
 
-    # 토핑을 하나씩 옮겨가면서 확인
-    while topping:
-        # 철수의 토핑에서 하나빼서
-        t = topping.popleft()  # t에 저장
-        cheolsu[t] -= 1 #해당 토핑 갯수 줄이기
-        if cheolsu[t] == 0:
-            del cheolsu[t] #개수가 0이 되면 딕셔너리에서 해당 토핑 제거
+    for t in topping:
+        bro.add(t)         # 현재 토핑을 동생의 집합에 추가
+        cheolsu[t] -= 1    # 해당 토핑의 개수를 1 감소시킴
 
-        # 동생의 토핑에 추가
-        if t in bro:
-            bro[t] += 1 #이미 해당 토핑이 있다면 개수 증가
-        else:
-            bro[t] = 1 #없었다면 1 새로 추가
-
-        # 공평하게 나눠진 경우를 체크
-        if len(cheolsu) == len(bro):
+        if cheolsu[t] == 0: # 만약 해당 토핑의 개수가 0이 되면
+            del cheolsu[t]  # 해당 토핑을 cheolsu에서 삭제
+            
+        # 공평하게 나눌 수 있는 경우 체크
+        if len(bro) == len(cheolsu):
             answer += 1
-        
-        # 동생의 토핑 종류가 철수보다 많아지면 종료
-        if len(bro) > len(cheolsu):
-            break
 
     return answer
